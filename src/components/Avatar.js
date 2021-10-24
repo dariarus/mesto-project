97fbab239da2ef95eb617d07998babde9802d315
Maria Aleksandrova <mail@aleksandrovamaria.ru>
2021-10-24 09:54:27 +0300

    fix: изменить появление кнопки редактирования профиля с js на css

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -3,20 +3,8 @@ export default class Avatar {
     this._avatar = user.avatar;
   }
 
-  _initImageAvatar() {
-    const buttonEditAvatar = document.querySelector('.profile__edit-avatar-button');
-
-    this._imageAvatar.addEventListener('mouseenter', () => {
-      buttonEditAvatar.classList.add('profile__edit-avatar-button_active');
-    });
-    buttonEditAvatar.addEventListener('mouseleave', () => {
-      buttonEditAvatar.classList.remove('profile__edit-avatar-button_active');
-    });
-  }
-
     setAvatar() {
       this._imageAvatar = document.querySelector('.profile__avatar');
       this._imageAvatar.src = this._avatar;
-      this._initImageAvatar();
     }
 }
